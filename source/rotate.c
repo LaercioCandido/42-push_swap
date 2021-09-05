e635e78f91298829f3fa49de5857b2c5f3eb31a6
laerciocandido <laercio@kunumi.com>
2021-09-05 13:27:36 -0300

    implementaçao de rotate.c

---
+++ source/rotate.c
@@ -0,0 +1,35 @@
+#include "../includes/push_swap.h"
+#include <stdio.h>
+
+static void	rotate_list(t_dlist **stack)
+{
+	t_dlist	*head;
+    int     number;
+
+    head = *stack;
+	if (head == NULL)
+		return ;
+    number = head->data;
+    ft_dlstadd_back(stack, ft_dlstnew(number));
+	head = head->next;
+	*stack = head;
+}
+
+void	rotate_a(t_stack *stack)
+{
+	rotate_list(&stack->a);
+	ft_putstr_fd("ra\n", 1);
+}
+
+void	rotate_b(t_stack *stack)
+{
+	rotate_list(&stack->b);
+	ft_putstr_fd("rb\n", 1);
+}
+
+void	rotate_both(t_stack *stack)
+{
+	rotate_list(&stack->a);
+	rotate_list(&stack->b);
+	ft_putstr_fd("rr\n", 1);
+}
