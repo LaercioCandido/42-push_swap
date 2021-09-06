24de07db5ed622367ff928d051606e15834f8fcb
laercio <lcandido@student.42sp.org.br>
2021-09-05 21:22:23 -0300

    implementaçao sort_five_nodes ok

--- source/sorting_b.c
+++ source/sorting_b.c
@@ -1,23 +1,26 @@
 #include "../includes/push_swap.h"
 #include <stdio.h>
 
-void    first_smallest(t_stack *stack)
+void    first_smallest(t_stack *stack, int qtt_nodes)
 {
+    int     i;
     int     position;
     int     smallest;
     t_dlist *current;
 
+    i = 2;
     position = 1;
-    current = stack->a;
-    smallest = current->data;
-    while (current->next != NULL)
+    smallest = stack->a->data;
+    current = stack->a->next;
+    while (i <= qtt_nodes)
     {
-        current = current->next;
         if (current->data < smallest)
         {
             smallest = current->data;
-            position++;
+            position = i;
         }
+        current = current->next;
+        i++;
     }
     printf("position smallest: %d\n", position);
     //push_smallest_to_b(stack, position);
@@ -26,7 +29,7 @@ void    first_smallest(t_stack *stack)
 
 void    sort_five_nodes(t_stack *stack)
 {
-    first_smallest(stack);
+    first_smallest(stack, 5);
     //second_smallest(stack);
     //sort_three_nodes(stack);
     //push_a(stack);
