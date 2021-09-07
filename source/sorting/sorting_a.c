92e1b0b61ecf37b47abe6f7ba3cef65ca8eda87a
laerciocandido <laercio@kunumi.com>
2021-09-06 21:12:44 -0300

    norma sorting_a.c

--- source/sorting/sorting_a.c
+++ source/sorting/sorting_a.c
@@ -1,31 +1,31 @@
 #include "../../includes/push_swap.h"
 
-void    sort_three_nodes(t_stack *stack)
+void	sort_three_nodes(t_stack *stack)
 {
-    int first;
-    int second;
-    int third;
-    t_dlist *pointer;
+	int first;
+	int second;
+	int third;
+	t_dlist *pointer;
 
-    pointer = stack->a->next;
-    first = pointer->previous->data;
-    second = pointer->data;
-    third = pointer->next->data;
-    if ((first < second && first < third && second > third) ||
-        (first > second && first < third && second < third) ||
-        (first > second && first > third && second > third))
-        swap_a(stack);
-    pointer = stack->a->next;
-    first = pointer->previous->data;
-    second = pointer->data;
-    third = pointer->next->data; 
-    if (first > second && first > third && second < third)
-        rotate_a(stack);
-    if (first < second && first > third && second > third)
-        reverse_rotate_a(stack);
+	pointer = stack->a->next;
+	first = pointer->previous->data;
+	second = pointer->data;
+	third = pointer->next->data;
+	if ((first < second && first < third && second > third) ||
+		(first > second && first < third && second < third) ||
+		(first > second && first > third && second > third))
+		swap_a(stack);
+	pointer = stack->a->next;
+	first = pointer->previous->data;
+	second = pointer->data;
+	third = pointer->next->data;
+	if (first > second && first > third && second < third)
+		rotate_a(stack);
+	if (first < second && first > third && second > third)
+		reverse_rotate_a(stack);
 }
 
-int     stack_ordered(t_stack *stack, int size)
+int		stack_ordered(t_stack *stack, int size)
 {
 	int		i;
 	int		number;
@@ -47,14 +47,14 @@ int     stack_ordered(t_stack *stack, int size)
 
 void	sort_stack(t_stack *stack, int qtt_node)
 {
-    if (stack_ordered(stack, qtt_node) == 0)
-        return ;
-    else if (qtt_node == 2)
-        swap_a(stack);
-    else if (qtt_node == 3)
-        sort_three_nodes(stack);
-    else if (qtt_node <= 5)
-        sort_five_nodes(stack, qtt_node);
-    else
-        radix_sort(stack);
+	if (stack_ordered(stack, qtt_node) == 0)
+		return ;
+	else if (qtt_node == 2)
+		swap_a(stack);
+	else if (qtt_node == 3)
+		sort_three_nodes(stack);
+	else if (qtt_node <= 5)
+		sort_five_nodes(stack, qtt_node);
+	else
+		radix_sort(stack);
 }
